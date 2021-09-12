a0ea6fd668c8867d8e66dae9d11d8d47826470dc
filliperomero <fillipe.romero@gmail.com>
2021-09-12 00:23:27 -0300

    chore: add users page and create users page

---
+++ src/components/Pagination/index.tsx
@@ -0,0 +1,22 @@
+import { Stack, HStack, Box } from '@chakra-ui/react'
+
+import PaginationItem from './PaginationItem'
+
+const Pagination = () => {
+  return (
+    <Stack direction={["column", "row"]} mt="8" justify="space-between" align="center" spacing="6">
+      <Box>
+        <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
+      </Box>
+      <HStack spacing="2">
+        <PaginationItem number={1} isCurrent />
+        <PaginationItem number={2} />
+        <PaginationItem number={3} />
+        <PaginationItem number={4} />
+        <PaginationItem number={5} />
+      </HStack>
+    </Stack>
+  )
+}
+
+export default Pagination
